a76d4e5539b569393542392248373430e56ed894
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-04-12 23:31:18 +0300

    Добавьте файлы проекта.

---
+++ Laba_18_OOP/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Laba_18_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DoTask1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DoTask2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1118, 455);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.DoTask1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1110, 426);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button2.Location = new System.Drawing.Point(944, -4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 92);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Завдання";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DoTask1
+            // 
+            this.DoTask1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DoTask1.Location = new System.Drawing.Point(-4, 252);
+            this.DoTask1.Name = "DoTask1";
+            this.DoTask1.Size = new System.Drawing.Size(1114, 171);
+            this.DoTask1.TabIndex = 2;
+            this.DoTask1.Text = "Виконати завдання";
+            this.DoTask1.UseVisualStyleBackColor = true;
+            this.DoTask1.Click += new System.EventHandler(this.DoTask1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть масив чисел\r\n(кожне нове число \r\nвиділяйте пробілом)";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.DoTask2);
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1110, 426);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Завдання 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button4.Location = new System.Drawing.Point(945, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 114);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Завдання";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(620, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(550, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введіть номер стовпця значення якого потрібно вивести";
+            // 
+            // DoTask2
+            // 
+            this.DoTask2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DoTask2.Location = new System.Drawing.Point(-4, 219);
+            this.DoTask2.Name = "DoTask2";
+            this.DoTask2.Size = new System.Drawing.Size(1114, 211);
+            this.DoTask2.TabIndex = 0;
+            this.DoTask2.Text = "Виконати завдання";
+            this.DoTask2.UseVisualStyleBackColor = true;
+            this.DoTask2.Click += new System.EventHandler(this.DoTask2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button DoTask1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button DoTask2;
+    }
+}
+
---
+++ Laba_18_OOP/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_18_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("В одномірному масиві, що складається з n дійсних елементів, розрахувати:\n а) кількість додатних елементів масиву;\n б) суму елементів масиву, розташованих після останнього елементу, рівного нулю. \nПеретворити масив таким чином, щоб спочатку розташовувались усі елементи, ціла частина яких не перевищує 1, а потім – усі інші. \nРезультати всіх розрахунків і перетворень масиву вивести на консоль.");
+        }
+
+        private void DoTask1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace(".", ","); 
+            try
+            {
+                double[] array = textBox1.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(x => double.Parse(x))
+                                                .ToArray();
+
+
+                // Розрахунок кількості додатних елементів масиву
+                int positiveCount = array.Where(x => x > 0).Count();
+
+                // Розрахунок суми елементів масиву, розташованих після останнього елементу, рівного нулю
+                double sumAfterLastZero = 0;
+                int lastIndex = Array.LastIndexOf(array, 0);
+                if (lastIndex != -1 && lastIndex != array.Length - 1)
+                {
+                    sumAfterLastZero = array.Skip(lastIndex + 1).Sum();
+                }
+
+                // Перетворення масиву
+                double[] transformedArray = array.OrderBy(x => Math.Truncate(x) <= 1 ? 0 : 1)
+                                                 .ThenBy(x => x)
+                                                 .ToArray();
+
+                // Виведення результатів в MessageBox
+                string resultMessage = $"Кількість додатних елементів масиву: {positiveCount}\n"
+                                       + $"Сума елементів масиву, розташованих після останнього елементу, рівного нулю: {sumAfterLastZero}\n\n"
+                                       + $"Перетворений масив: {string.Join(", ", transformedArray)}";
+                MessageBox.Show(resultMessage, "Результати");
+            }
+            catch (Exception) 
+            {
+                MessageBox.Show("Будь-ласка перевірте введені дані!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Даний двовимірний масив. Вивести на екран:\n а) увесь масив;\n б) усі елементи п’ятого рядка масиву;\n в) усі елементи s - го стовбця масиву.");
+        }
+
+        private void DoTask2_Click(object sender, EventArgs e)
+        {
+            int[,] array = new int[,]
+            {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                {21, 22, 23, 24, 25}
+            };
+
+            // Виведення усього масиву
+            string arrayString = "Увесь масив:\n";
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    arrayString += array[i, j] + "\t";
+                }
+                arrayString += "\n";
+            }
+
+            // Виведення елементів п'ятого рядка масиву
+            string fifthRowString = "Елементи п'ятого рядка масиву:\n";
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                fifthRowString += array[4, j] + "\t";
+            }
+
+            // Виведення елементів s-го стовбця масиву
+            int s = int.Parse(textBox2.Text);
+            string sColumnString = $"Елементи {s}-го стовбця масиву:\n";
+           try
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    sColumnString += array[i, s - 1] + "\n";
+                }
+            }
+           catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Такого стовпця масиву не існує!!!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // Виведення результатів в MessageBox
+            string resultMessage = $"{arrayString}\n{fifthRowString}\n{sColumnString}";
+            MessageBox.Show(resultMessage, "Результати");
+        }
+    }
+}
